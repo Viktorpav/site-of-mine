56d69608abcf69716d0ed5f5041a439091cc0f63
Viktorpav <doncar2003@gmail.com>
2022-12-09 16:37:55 +0100

    Init commit 2

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 # In case of git host provider switch
 git remote remove origin
-git remote add origin <url here>
+git remote add origin <url here>  #https://git-codecommit.eu-central-1.amazonaws.com/v1/repos/site-of-mine
