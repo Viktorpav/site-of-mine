61ccec10f792e66e9b4ed714b0a63f7d2fba97d6
Viktorpav <doncar2003@gmail.com>
2023-05-01 00:55:43 +0200

    Pulling videos from local to S3

--- s3sync.sh
+++ s3sync.sh
@@ -1,5 +1,5 @@
 # Copy static site to S3 bucket
-aws s3 sync . s3://$1 --exclude "*" --include index.html --include 404.html --include 503.html --include "video/*"
+aws s3 sync . s3://$1 --exclude "*" --include index.html --include 404.html --include 503.html --include "video/*" --include "lambda-layers/*"
 
 # Clear CloudFront cache
 DistributionId=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='$1'].id" --output text)
